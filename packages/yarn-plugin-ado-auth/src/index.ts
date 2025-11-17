402c9e64f1fb8ef4d68deecb2100e3884667e1b6
Jason Morse <jasonmo@microsoft.com>
2025-11-17 14:13:56 -0800

    change to @microsoft scope, split TokenCache to separate file

--- packages/yarn-plugin-ado-auth/src/index.ts
+++ packages/yarn-plugin-ado-auth/src/index.ts
@@ -1,6 +1,11 @@
-export type { GetAuthHeaderOptions } from "./getToken.ts";
-export { TokenCache, getToken } from "./getToken.ts";
 export type { AuthPluginConfigurationValueMap } from "./configuration.ts";
 export { getConfiguration, loadConfiguration } from "./configuration.ts";
+
+export type { GetAuthHeaderOptions } from "./getToken.ts";
+export { getToken } from "./getToken.ts";
+
 import plugin from "./plugin.ts";
+
+export { TokenCache } from "./tokenCache.ts";
+
 export { plugin };
