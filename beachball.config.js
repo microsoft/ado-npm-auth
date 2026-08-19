94c633e2d72f3026a6c9f40646079d86f578c1f2
Elizabeth Craig <elcraig@microsoft.com>
2026-08-19 00:57:04 +0000

    Update publish github auth to use persisted token (#190)

--- beachball.config.js
+++ beachball.config.js
@@ -3,6 +3,8 @@
 const config = {
   changehint: 'Run "yarn change" to generate a change file',
   branch: "main",
+  // The current publishing setup skips the step that creates git tags, so make it explicit that they're not created
+  gitTags: false,
   groupChanges: true,
   ignorePatterns: ["**/*.test.ts"],
   disallowedChangeTypes: [
